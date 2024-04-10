5e79d15e36c0d2692890d77a468b957cc0810e66
josh-lum <joshua.lum@outlook.com>
2024-04-10 16:54:05 -0700

    added a basic on click listener (in comments) for generation textfile obtainer

--- app/src/main/java/com/example/project2/pokemonImporter.java
+++ app/src/main/java/com/example/project2/pokemonImporter.java
@@ -1,10 +1,14 @@
 package com.example.project2;
 
+import android.content.Context;
+import android.view.View;
+
 public class pokemonImporter {
     // push to github to test branch
     // writing line 5 test
 
     // put button here to have user input a generation of pokemon 1-3:
-    int generation;
+    int generationNumber;
 
+    String generation;
 }
