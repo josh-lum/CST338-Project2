67bb184ef20c3a41e7dda63310f12232a92c2fd1
josh-lum <joshua.lum@outlook.com>
2024-04-10 16:18:59 -0700

    Added Generation1.txt, Generation2.txt, and Generation3.txt textfiles to be used for starter selection

--- app/src/main/java/com/example/project2/pokemonImporter.java
+++ app/src/main/java/com/example/project2/pokemonImporter.java
@@ -4,4 +4,7 @@ public class pokemonImporter {
     // push to github to test branch
     // writing line 5 test
 
+    // put button here to have user input a generation of pokemon 1-3:
+    int generation;
+
 }
