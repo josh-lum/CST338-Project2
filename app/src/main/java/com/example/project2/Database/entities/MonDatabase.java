d97a90b2c46bd5d9430cc6dbad3c42d451a1d0b7
josh-lum <joshua.lum@outlook.com>
2024-04-16 19:26:30 -0700

    added room dependencies
    added MonDatabase.java, with mon attributes

---
+++ app/src/main/java/com/example/project2/Database/entities/MonDatabase.java
@@ -0,0 +1,10 @@
+package com.example.project2.Database.entities;
+
+public class MonDatabase {
+    private String name;
+    private String type;
+    private String moves;
+    private int xp;
+    private int damage;
+    private int attack;
+}
