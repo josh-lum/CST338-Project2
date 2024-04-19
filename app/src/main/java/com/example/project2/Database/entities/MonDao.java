53886aa6086ca0d99e1efad7c088ffbaff91d2bc
qkrommenhoek <qkrommenhoek@gmail.com>
2024-04-18 18:31:39 -0700

    Mondatabase and mon.java also user.java

--- app/src/main/java/com/example/project2/Database/entities/MonDao.java
+++ app/src/main/java/com/example/project2/Database/entities/MonDao.java
@@ -1,10 +1,17 @@
 package com.example.project2.Database.entities;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.project2.Database.Mon;
 
 @Dao
 public interface MonDao {
     @Insert(onConflict =OnConflictStrategy.REPLACE)
-    void
+    void Insert(Mon mon);
+
+    @Query("Select * from " + MonDatabase.monTable)
+    List<Mon> getAllRecords();
 }
