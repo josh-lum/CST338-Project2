b61ae7569bf6dd06858a0529360795cf12a68f16
josh-lum <joshua.lum@outlook.com>
2024-04-20 21:09:16 -0700

    changing 8 files, LandingPage.java and user_screen.xml most changes made

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_screen);
 
         /* need to change more in future but:
 
