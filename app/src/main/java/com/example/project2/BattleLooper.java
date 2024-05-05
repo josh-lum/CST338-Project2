264875450e8afe87fd5a63d74e651917c5b3cbe2
josh-lum <joshua.lum@outlook.com>
2024-05-05 13:48:49 -0700

    updated battle Looper to correctly display a sprite
    added a method to Opponent and pokemon
    
    added Pokemonsprites (most likely going to deprecate)
    
    added league champ sprite

--- app/src/main/java/com/example/project2/BattleLooper.java
+++ app/src/main/java/com/example/project2/BattleLooper.java
@@ -13,7 +13,9 @@ import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class BattleLooper extends AppCompatActivity {
 
@@ -25,6 +27,16 @@ public class BattleLooper extends AppCompatActivity {
     private Opponent opponent;
     private User user;
     private BattleScreenBinding binding;
+    private ImageView bulbasaurImageView;
+    private ImageView charmanderImageView;
+    private ImageView squirtleImageView;
+    private ImageView chikoritaImageView;
+    private ImageView cyndaquilImageView;
+    private ImageView totodileImageView;
+    private ImageView treeckoImageView;
+    private ImageView torchicImageView;
+    private ImageView mudkipImageView;
+    private int localAccumulator = 0;
 
 
     @Override
@@ -32,10 +44,21 @@ public class BattleLooper extends AppCompatActivity {
         super.onCreate(savedInstance);
         opponent = new Opponent();
         binding = com.example.project2.databinding.BattleScreenBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
         handler = new Handler(Looper.getMainLooper());
+        setContentView(binding.getRoot());
 
-        // go back to menu screen
+        // sprite loading
+        bulbasaurImageView = findViewById(R.id.bulbasaurImageView);
+        charmanderImageView = findViewById(R.id.charmanderImageView);
+        squirtleImageView = findViewById(R.id.squirtleImageView);
+        chikoritaImageView = findViewById(R.id.chikoritaImageView);
+        cyndaquilImageView = findViewById(R.id.cyndaquilImageView);
+        totodileImageView = findViewById(R.id.totodileImageView);
+        treeckoImageView = findViewById(R.id.treeckoImageView);
+        torchicImageView = findViewById(R.id.torchicImageView);
+        mudkipImageView = findViewById(R.id.mudkipImageView);
+
+        // go back to login screen
         Button backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,6 +83,9 @@ public class BattleLooper extends AppCompatActivity {
             @SuppressLint("SetTextI18n")
             @Override
             public void run() {
+                int start = 0;
+                int resId = updateSprite(start);
+                binding.oppCircle.setImageResource(resId);
                if(opponent.hasMorePokemon()){
                    Pokemon currentMon = opponent.getNextPokemon();
                    if(currentMon.getHp() <= 0){
@@ -76,7 +102,7 @@ public class BattleLooper extends AppCompatActivity {
         }, damageInterval);
     }
     private void updateOpponentPokemonHealth(){
-        opponent.getNextPokemon().getHp();
+
     }
 
     private void inflictDamage(){
@@ -84,12 +110,50 @@ public class BattleLooper extends AppCompatActivity {
             Pokemon currentMon = opponent.getNextPokemon();
             currentMon.setDmg(DAMAGE_TICK);
             updateOpponentPokemonHealth();
+            Toast.makeText(this, "did damage!", Toast.LENGTH_SHORT).show();
+        }else{
+
         }
     }
     public void onBackPress(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public int updateSprite(int number){
+
+        if(number == 0){
+            localAccumulator += 1;
+            return R.drawable.bulbasaur;
+        }else if(number == 1){
+            localAccumulator += 1;
+            return R.drawable.charmander;
+        }else if(number == 2){
+            localAccumulator += 1;
+            return R.drawable.squirtle;
+        }else if(number == 3){
+            localAccumulator += 1;
+            return R.drawable.chikorita;
+        }else if(number == 4){
+            localAccumulator += 1;
+            return R.drawable.cyndaquil;
+        }
+        else if(number == 5){
+            localAccumulator += 1;
+            return R.drawable.totodile;
+        }else if(number == 6){
+            localAccumulator += 1;
+            return R.drawable.treecko;
+        }else if(number == 7){
+            localAccumulator += 1;
+            return R.drawable.torchic;
+        }else if(number == 8){
+            localAccumulator += 1;
+            return R.drawable.mudkip;
+        }else{
+            return R.drawable.leaguechamp;
+        }
     }
 
 }
