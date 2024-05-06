fab23586f3c694a807a7f072698f27a5a242fa74
josh-lum <joshua.lum@outlook.com>
2024-05-05 19:14:11 -0700

    changed gradle build to incorporate tests and unit tests

---
+++ app/src/main/java/com/example/project2/UnitTests.java
@@ -0,0 +1,32 @@
+package com.example.project2;
+
+
+import android.view.View;
+import android.widget.Button;
+
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
+public class UnitTests {
+
+    // Joshua Lum's tests
+    @Test
+    public void logout(){
+
+    }
+
+    @Test
+    public void damage(){
+
+    }
+
+
+
+
+
+
+    // Quinn Krommenhoek's tests
+}
