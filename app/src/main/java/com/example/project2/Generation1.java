e470382d7802c7b929a34e567e70886ab7c2f55e
qkrommenhoek <qkrommenhoek@gmail.com>
2024-05-05 22:32:05 -0700

    adding party works

--- app/src/main/java/com/example/project2/Generation1.java
+++ app/src/main/java/com/example/project2/Generation1.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -21,6 +22,7 @@ public class Generation1 extends AppCompatActivity {
 
     private MonDao monDao;
     private MonRepository repository;
+    private int loggedInUserId = -1;
 
 
     Button backButton;
@@ -31,42 +33,45 @@ public class Generation1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = com.example.project2.databinding.Generation1Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        logInUser(savedInstanceState);
+        SharedPreferences sharedPreferences = getSharedPreferences(MainActivity.SHARED_PREFERENCE_USERID_VALUE, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        int userId = sharedPreferences.getInt(MainActivity.SHARED_PREFERENCE_USERID_KEY, -1);
 
-        Intent intent = getIntent();
-        int userId = intent.getIntExtra(MainActivity.MAIN_ACTIVITY_USER_ID, -1);
+
+//        Intent intent = getIntent();
+//        int userId = intent.getIntExtra(MainActivity.MAIN_ACTIVITY_USER_ID, -1);
         repository = MonRepository.getRepository(getApplication());
         binding.bulbasaurButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Mon mon = new Mon("BULBASAUR",0,0, 10);
-//                repository.insertMon(mon);
-//                Party party = new Party(userId,mon.getIdNumber());
-//                repository.insertParty(party);
-                startActivity(MainActivity.MainActivityIntentFactory(getApplicationContext(),userId));
+                Mon mon = new Mon("BULBASAUR",0,0, 10);
+                int monId = (int)repository.insertMon(mon);
+                Party party = new Party(loggedInUserId,monId);
+                repository.insertParty(party);
+                startActivity(MainActivity.MainActivityIntentFactory(getApplicationContext(),loggedInUserId));
             }
         });
         binding.charmanderButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(MainActivity.MainActivityIntentFactory(getApplicationContext(),userId));
+                Mon mon = new Mon("CHARMANDER",0,0, 10);
+                int monId = (int)repository.insertMon(mon);
+                Party party = new Party(loggedInUserId,monId);
+                repository.insertParty(party);
+                startActivity(MainActivity.MainActivityIntentFactory(getApplicationContext(),loggedInUserId));
             }
         });
         binding.squirtleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(MainActivity.MainActivityIntentFactory(getApplicationContext(),userId));
+                startActivity(MainActivity.MainActivityIntentFactory(getApplicationContext(),loggedInUserId));
             }
         });
 
 //        Intent intent = getIntent();
 //        int userId = intent.getIntExtra(MainActivity.SHARED_PREFERENCE_USERID_VALUE, -1);
 
-        binding.bulbasaurButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
 
         backButton = findViewById(R.id.backButtonGen1);
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -79,9 +84,29 @@ public class Generation1 extends AppCompatActivity {
 
 
     }
+    private void logInUser(Bundle savedInstanceState){
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(MainActivity.SHARED_PREFERENCE_USERID_KEY,
+                Context.MODE_PRIVATE);
+        if(sharedPreferences.contains(MainActivity.SHARED_PREFERENCE_USERID_KEY)){
+            loggedInUserId = sharedPreferences.getInt(MainActivity.SHARED_PREFERENCE_USERID_VALUE, -1);
+        }
+        if(loggedInUserId==-1 & savedInstanceState!=null&&savedInstanceState.containsKey(MainActivity.SAVED_INSTANCE_STATE_USERID_KEY)){
+            loggedInUserId = savedInstanceState.getInt(MainActivity.SAVED_INSTANCE_STATE_USERID_KEY,-1);
+        }
+        if(loggedInUserId==-1) {
+            loggedInUserId = getIntent().getIntExtra(MainActivity.MAIN_ACTIVITY_USER_ID, -1);
+        }
+        if(loggedInUserId==-1){
+            return;
+        }
+
+
+
+
+    }
     static Intent Generation1IntentFactory(Context context, int userId){
         Intent intent = new Intent(context, Generation1.class);
-        intent.putExtra(MainActivity.SHARED_PREFERENCE_USERID_KEY, userId);
+        intent.putExtra(MainActivity.MAIN_ACTIVITY_USER_ID, userId);
         return intent;
     }
 }
